60080c818ca6b757b2472b30d0063dad185f5f09
nicholas-p1 <contact@nicholaspfaff.ch>
2021-02-25 16:16:40 +0000

    Add frame generation step to compiler.cpp

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -12,6 +12,13 @@ int main()
         AST *ast = parseAST();
         std::cerr << "Parsing Works!" << std::endl;
 
+        // global frame
+        Frame* globalFrame = new Frame();
+
+        // pre-process AST to generate Frame objects
+        ast->generateFrames(globalFrame);
+        std::cerr << "Frame Generation Works!" << std::endl;
+
         // write MIPS assembly to stdout
         ast->compile(std::cout);
         std::cerr << "Compiling Works!" << std::endl;
