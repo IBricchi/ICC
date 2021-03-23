b45a695a925fb478cdb4ca691306bb4313a4afaa
nicholas-p1 <contact@nicholaspfaff.ch>
2021-03-23 17:13:36 +0000

    Add more complicated global variable test cases

---
+++ compiler_tests/local_var/global_types.c
@@ -0,0 +1,25 @@
+int a = 22;
+float b = 121.5f;
+double c = 234.567;
+char d = '7';
+unsigned e = 11;
+
+int intArr[10];
+
+int g()
+{
+    a = a+1;
+    b = b+1.0f;
+    c = c+1.0;
+    d = d+1;
+    e = e+1;
+
+    intArr[5] = 789;
+
+    return  (a == 23) && 
+            (b == 122.5f) &&
+            (c == 235.567) &&
+            (d == '8') &&
+            (e == 12) &&
+            (intArr[5] == 789);
+}
